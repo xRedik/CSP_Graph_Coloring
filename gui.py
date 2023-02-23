f0322336e6342fef42c1832712bdf940a0dab643
Farid <feridquliyev8@gmail.com>
2023-02-23 11:32:06 +0400

    GUI class was added

--- gui.py
+++ gui.py
@@ -1,60 +1,89 @@
 import pygame
-import gui_config
 import math
+import sys
 
-def add_edges(node_class_lists):
-    for node in node_class_lists:
-        for path in node.paths:
-            pygame.draw.line(gui_config.window, (0,0,0), node.position, _get_pos_from_value(node_class_lists,path))
+class GUI():
+    def __init__(self,vertex_list,resolution = (1280,720), vertex_radius = 20, 
+                 boundary_x = 150, boundary_y = 50, fps = 60, font_size = 15):
+        self.vertex_list = vertex_list
+        self.resolution = resolution
+        self.vertex_radius = vertex_radius
+        self.boundary_x, self.boundary_y = boundary_x, boundary_y
+        self.fps = fps
+        self.font_size = font_size
+        self.lmb_state = False
+        pygame.init()
+        self.window = pygame.display.set_mode(self.resolution)
+        pygame.display.set_caption("CSP Graph Coloring by Farid Guliyev")
+        self.clock = pygame.time.Clock()
+        self.window.fill((255, 255, 255))
 
-def _get_pos_from_value(node_class_lists,path):
-    for node in node_class_lists:
-        if node.value == path:
-            return node.position
+    def check_events(self):
+        for e in pygame.event.get():
+            if e.type == pygame.MOUSEBUTTONDOWN:
+                if e.button == pygame.BUTTON_LEFT:
+                     self.lmb_state = True
+            if e.type == pygame.MOUSEBUTTONUP:
+                if e.button == pygame.BUTTON_LEFT:
+                     self.lmb_state = False
+            if e.type == pygame.QUIT:
+                sys.exit()
+                
 
-def add_vertex(node_class_lists,lmb):
-    new_x, new_y = pygame.mouse.get_pos()
-    for node in node_class_lists:
-        br = pygame.draw.circle(gui_config.window, (0,0,0), node.position, gui_config.vertex_radius)
-        _add_name(node, gui_config.window, size=15)
-        
-        print(new_x,new_y)
-        #DEYIW
-        if br.collidepoint(new_x, new_y) and lmb:
-            node.clicked = True
-        if node.clicked and not lmb:
-            node.clicked = False
-
-        if node.clicked:
-            node.pos_x = new_x
-            node.pos_y = new_y
-
-
-def _add_name(node, window, size=15):
-    font = pygame.font.Font(pygame.font.get_default_font(), size)
-    w, h = font.size(str(node.value))
-    surf = font.render(str(node.value), True, (255,255,255))
-    window.blit(surf, (node.pos_x-(w/2), 
-                       node.pos_y-(h/2)))
-
-
-def make_graph_flexible(node_class_lists):
-    for i, node_1 in enumerate(node_class_lists):
-        for j, node_2 in enumerate(node_class_lists):
-            if i == j:
-                continue
-            
-            dx, dy = node_2.pos_x - node_1.pos_x, node_2.pos_y - node_1.pos_y
-            dist = math.hypot(dx, dy) + 1
-            
-            if dist < gui_config.vertex_radius + 50:
-                # forceX = dx / dist
-                # forceY = dy / dist
-                node_1.pos_x -= int(node_1.repel * (dx / dist))
-                node_1.pos_y -= int(node_1.repel * (dy / dist))
+    def update_graph(self):
+        self.add_edges()
+        self.add_vertex()
+        self.make_graph_flexible()
     
-        node_1.position = (node_1.pos_x, node_1.pos_y)
+    def add_edges(self):
+        for vertex in self.vertex_list:
+            for path in vertex.paths:
+                pygame.draw.line(self.window, (0,0,0), vertex.position, self._get_pos_from_value(path))
 
+    def _get_pos_from_value(self,path):
+        for vertex in self.vertex_list:
+            if vertex.value == path:
+                return vertex.position
 
-def refresh_window():
-    gui_config.window.fill((255, 255, 255))
+    def add_vertex(self):
+        new_x, new_y = pygame.mouse.get_pos()
+        for vertex in self.vertex_list:
+            br = pygame.draw.circle(self.window, (0,0,0), vertex.position, self.vertex_radius)
+            self._add_name(vertex)
+            
+            #DEYIW
+            if br.collidepoint(new_x, new_y) and self.lmb_state:
+                vertex.clicked = True
+            if vertex.clicked and not self.lmb_state:
+                vertex.clicked = False
+
+            if vertex.clicked:
+                vertex.pos_x = new_x
+                vertex.pos_y = new_y
+
+    def _add_name(self,vertex):
+        font = pygame.font.Font(pygame.font.get_default_font(), self.font_size)
+        w, h = font.size(str(vertex.value))
+        surf = font.render(str(vertex.value), True, (255,255,255))
+        self.window.blit(surf, (vertex.pos_x-(w/2), 
+                        vertex.pos_y-(h/2)))
+
+    def make_graph_flexible(self):
+        for i, vertex_1 in enumerate(self.vertex_list):
+            for j, vertex_2 in enumerate(self.vertex_list):
+                if i == j:
+                    continue
+                
+                dx, dy = vertex_2.pos_x - vertex_1.pos_x, vertex_2.pos_y - vertex_1.pos_y
+                euc_distance = math.hypot(dx, dy) + 1
+                
+                if euc_distance < self.vertex_radius + 50:
+                    vertex_1.pos_x -= int(vertex_1.repel * (dx / euc_distance))
+                    vertex_1.pos_y -= int(vertex_1.repel * (dy / euc_distance))
+        
+            vertex_1.position = (vertex_1.pos_x, vertex_1.pos_y)
+
+    def refresh_window(self):
+        self.clock.tick(self.fps)
+        pygame.display.flip()
+        self.window.fill((255, 255, 255))
--- main.py
+++ main.py
@@ -1,32 +1,16 @@
 from read_initialize import *
-from gui_config import *
-import gui
-import sys
+from gui import GUI
 
 def main():
     num_colors, node_class_lists = read_file("input_text.txt")
     color_list = choose_colors(num_colors)
-    left_click = False
-    while True:
+    gui = GUI(node_class_lists)
+    while True:        
+        gui.check_events()
+        gui.update_graph()
         gui.refresh_window()
-        for e in pygame.event.get():
-            if e.type == pygame.MOUSEBUTTONDOWN:
-                if e.button == pygame.BUTTON_LEFT:
-                    left_click = True
-            if e.type == pygame.MOUSEBUTTONUP:
-                if e.button == pygame.BUTTON_LEFT:
-                    left_click = False
-            if e.type == pygame.QUIT:
-                sys.exit()
-        
-        gui.make_graph_flexible(node_class_lists)
-        gui.add_edges(node_class_lists)
-        gui.add_vertex(node_class_lists,left_click)
-  
-        clock.tick(fps)
-        pygame.display.flip()
 
-    #csp(num_colors,dict_graph)
+        #csp(num_colors,dict_graph)
 
 
 if __name__ == "__main__":
